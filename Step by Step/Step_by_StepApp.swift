574479af624414ab1459c1ac547bf1367664bff3
Gustavo Vazquez <great.visions@icloud.com>
2024-11-21 22:22:55 -0800

    Initial commit
    Initial commit - testing upload to Github

---
+++ Step by Step/Step_by_StepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_by_StepApp.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_by_StepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DashboardView()
+        }
+    }
+}
---
+++ Step by Step/View/DashboardView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 10/4/24.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    DashboardView()
+}
