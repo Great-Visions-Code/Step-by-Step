bfb1e55eba3b439320c02a9f60945e979250a3ef
Gustavo Vazquez <great.visions@icloud.com>
2024-11-27 20:51:26 -0800

    Create AchievementsView.swift
    Placeholder view

---
+++ Step by Step/View/AchievementsView.swift
@@ -0,0 +1,21 @@
+//
+//  AchievementsView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 11/27/24.
+//
+
+import SwiftUI
+
+struct AchievementsView: View {
+    var body: some View {
+        // TODO: The entire page
+        Text("Achievements Page")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    AchievementsView()
+}
