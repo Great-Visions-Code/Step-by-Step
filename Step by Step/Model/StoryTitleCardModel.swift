d6acb98f5c359320e33db4ae8bcf79eebe907541
Gustavo Vazquez <great.visions@icloud.com>
2024-11-27 20:51:05 -0800

    Create StoryTitleCardModel.swift
    Story title card variables

---
+++ Step by Step/Model/StoryTitleCardModel.swift
@@ -0,0 +1,15 @@
+//
+//  StoryTitleCardModel.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 11/27/24.
+//
+
+import SwiftUI
+
+// Represents a story title card in the "Choose Your Adventure" section.
+struct StoryTitleCard {
+    let title: String
+    let color: Color
+    let completion: Int
+}
