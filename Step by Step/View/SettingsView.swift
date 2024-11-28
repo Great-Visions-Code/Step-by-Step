44c83c74384dc99a529d84674ae3d17a501e5eca
Gustavo Vazquez <great.visions@icloud.com>
2024-11-27 20:53:13 -0800

    Create SettingsView.swift
    Placeholder view

---
+++ Step by Step/View/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 11/27/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings Page")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    SettingsView()
+}
