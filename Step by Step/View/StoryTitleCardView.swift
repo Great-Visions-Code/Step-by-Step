702e07663dd297b156500e60981b554237164b9b
Gustavo Vazquez <great.visions@icloud.com>
2024-12-01 17:42:23 -0800

    Update StoriesTitleCardViewModel.swift
    Created text for completed percentage to display.
    Bind selected story with sheet presentation

--- Step by Step/View/StoryTitleCardView.swift
+++ Step by Step/View/StoryTitleCardView.swift
@@ -7,10 +7,12 @@
 
 import SwiftUI
 
-// Reusable view for story title cards
 struct StoryTitleCardView: View {
-    var title: String
-    var color: Color
+    // Story data to display
+    var story: StoryTitleCard
+    // Selected story to show in Sheet
+    @Binding var selectedStory: StoryTitleCard?
+    @Binding var isSheetPresented: Bool
     
     // State variable for scaling animation
     @State private var isPressed = false
@@ -18,28 +20,42 @@ struct StoryTitleCardView: View {
     var body: some View {
         VStack {
             // Title cards format
-            Text(title)
+            // Display story title
+            Text(story.title)
                 .font(.headline)
                 .multilineTextAlignment(.center)
                 .foregroundStyle(.white)
+                .lineLimit(2)
+                // Scales longer story titles
+                .minimumScaleFactor(0.8)
                 .padding()
-                .frame(width: 150, height: 150)
-                .background(color)
-                .cornerRadius(20)
-                .scaleEffect(isPressed ? 0.95 : 1.0)
-                .animation(.easeInOut(duration: 0.1), value: isPressed)
-                .onTapGesture {
-                    // Animate on press
-                    withAnimation {
-                        isPressed.toggle()
-                    }
-                    
-                    // Toggle back after a short delay (0.2 sec)
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                        withAnimation {
-                            isPressed.toggle()
-                    }
-                }
+            
+            // Display completion %
+            Text("Completed: \(story.completion)%")
+                .font(.subheadline)
+                .foregroundStyle(.white.opacity(0.8))
+        }
+        .padding()
+        // MARK: Frame for gesture actions GV 12/1/24
+        .frame(width: 150, height: 150)
+        .background(story.color)
+        .cornerRadius(20)
+        // MARK: Press gesture animation GV 12/1/24
+        .scaleEffect(isPressed ? 0.95 : 1.0)
+        .animation(.easeInOut(duration: 0.1), value: isPressed)
+        .onTapGesture {
+            // Animate on press
+            withAnimation {
+                isPressed.toggle()
+            }
+            // Toggle back after a short delay (0.2 sec)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                withAnimation {
+                    isPressed.toggle()
+            }
+                // MARK: Present Sheet with selected story GV 12/1/24
+                selectedStory = story
+                isSheetPresented = true
             }
         }
     }
@@ -47,6 +63,12 @@ struct StoryTitleCardView: View {
 
 #Preview {
     StoryTitleCardView(
-        title: "Survive Step by Step\n\nCompleted: 10%",
-        color: .green)
+        story: StoryTitleCard(
+            title: "Future Adventures",
+            color: .green,
+            completion: 100),
+        // Sheet will NOT present IN PREVIEW
+        selectedStory: .constant(nil),
+        isSheetPresented: .constant(false)
+    )
 }
