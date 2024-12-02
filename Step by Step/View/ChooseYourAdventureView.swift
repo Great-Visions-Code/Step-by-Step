34ce9db66c03b4af0de637dbb209e977266e405a
Gustavo Vazquez <great.visions@icloud.com>
2024-12-01 17:42:55 -0800

    Update ChooseYourAdventureView.swift
    Added sheet for story details

--- Step by Step/View/ChooseYourAdventureView.swift
+++ Step by Step/View/ChooseYourAdventureView.swift
@@ -7,44 +7,58 @@
 
 import SwiftUI
 
-// Sample data FOR PREVIEW
-let stories = [
-    StoryTitleCard(title: "Survive Step by Step",
-                   color: Color.green,
-                   completion: 10),
-    StoryTitleCard(title: "Future Adventures",
-                   color: Color.gray,
-                   completion: 0)
-]
-
 struct ChooseYourAdventureView: View {
     // Array from StoryTitleCardModel.swift
     var stories: [StoryTitleCard]
     
+    // State to manage the selected story and Sheet presentation
+    @State private var selectedStory: StoryTitleCard? = nil
+    @State private var isSheetPresented: Bool = false
+    
     var body: some View {
-        // Display Title
-        Text("Choose Your Adventure:")
-            .font(.title2)
-            .bold()
-            .padding(.top)
-        
-        // Horizontal scroll section for story title cards
-        ScrollView(.horizontal, showsIndicators: true) {
-            HStack(spacing: 15) {
-                // ForEach loop to generate title cards
-                ForEach(stories, id: \.title) { story in
-                    StoryTitleCardView(
-                        title: "\(story.title)\n\nComplete: \(story.completion)%",
-                        color: story.color
-                    )
+        VStack {
+            // Display Title
+            Text("Choose Your Adventure:")
+                .font(.title2)
+                .bold()
+                .padding(.top)
+            
+            // MARK: Horizontal scroll section for story title cards
+            ScrollView(.horizontal, showsIndicators: true) {
+                HStack(spacing: 15) {
+                    // ForEach loop to generate title cards
+                    ForEach(stories, id: \.title) { story in
+                        StoryTitleCardView(
+                            story: story,
+                            selectedStory: $selectedStory,
+                            isSheetPresented: $isSheetPresented
+                        )
+                    }
                 }
+                // Center title cards IN DashboardView.swift
+                .padding(.horizontal, 28)
             }
-            // Center title cards IN DashboardView.swift
-            .padding(.horizontal, 28)
+            .padding(.top)
         }
-        .padding(.top)
+        // MARK: Sheet presentation GV 12/1/24
+        .sheet(isPresented: $isSheetPresented) {
+            if let story = selectedStory {
+                StoryDetailSheetView(story: story)
+            }
+        }
     }
 }
+
+
+// Sample data FOR PREVIEW
+let stories = [
+    StoryTitleCard(title: "Survive",
+                   color: Color.green,
+                   completion: 100),
+    StoryTitleCard(title: "Future Adventures",
+                   color: Color.gray,
+                   completion: 0)
+]
 
 #Preview {
     ChooseYourAdventureView(stories: stories)
