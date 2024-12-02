52b1aaefee368fa06977e0f1ecbbfd7595ef81a1
Gustavo Vazquez <great.visions@icloud.com>
2024-12-01 21:59:36 -0800

    Create StoryHomeView.swift

---
+++ Step by Step/View/StoryHomeView.swift
@@ -0,0 +1,21 @@
+//
+//  StoryHomeView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/1/24.
+//
+
+import SwiftUI
+
+struct StoryHomeView: View {
+    var body: some View {
+        // TODO: Everything
+        Text("Story Home View Placeholder")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    StoryHomeView()
+}
