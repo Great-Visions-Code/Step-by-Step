e3fbf5b2baed550f01cdc80820e0f4423e164dbf
Gustavo Vazquez <great.visions@icloud.com>
2024-12-02 18:01:14 -0800

    Update StoriesTitleCardViewModel.swift

--- Step by Step/ViewModel/StoriesTitleCardViewModel.swift
+++ Step by Step/ViewModel/StoriesTitleCardViewModel.swift
@@ -17,13 +17,13 @@ class StoriesTitleCardViewModel: ObservableObject {
             title: "Survive",
             color: Color.green,
             completion: 10,
-            details: "In 'Survive - Step by Step', navigate a post-apocalyptic world while battling zombies and managing resources. Your choices shape your fate."
+            details: "In 'Survive,' you wake up to find your city plunged into chaos overnight. A zombie apocalypse has turned your once-familiar world into a dangerous and unpredictable battleground.\n\n Your goal is to survive for 10 days and reach the evacuation center at the city's edge, where safety awaits.\n\n Over the course of 10 real-world days, you’ll face tough choices and limited resources.\n Expect a thrilling, high-stakes adventure where death lurks at every corner, from zombie attacks to starvation, accidents, and hostile survivors. With multiple pathways to explore and few routes to ultimate survival, the challenge will test your resolve and encourage you to take steps—both in-game and in real life—to make it out alive."
         ),
         StoryTitleCard(
             title: "Future Adventures",
             color: Color.gray,
             completion: 0,
-            details: "Stay tuned for upcoming thrilling adventures that will test your strategy and creativity!"
+            details: "Stay tuned for upcoming thrilling adventures that will test your strategy!"
         )
     ]
 }
