c8c32b7d9fd386a71216a4e40b8de4a6e3a36aa6
Gustavo Vazquez <great.visions@icloud.com>
2024-12-05 21:31:46 -0800

    Create StoryTitleView.swift

---
+++ Step by Step/View/Story Home Views/StoryTitleView.swift
@@ -0,0 +1,25 @@
+//
+//  StoryTitle.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/5/24.
+//
+
+import SwiftUI
+
+struct StoryTitleView: View {
+    // Story title passed to this view
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding(.bottom, 55)    }
+}
+
+#Preview {
+    StoryTitleView(
+        title: "Survive"
+    )
+}
