5d3a03523f65db0f6b4f8caf931a7d5f3479f609
Gustavo Vazquez <great.visions@icloud.com>
2024-12-08 22:23:24 -0800

    Create StoryHomeNavigationButtonView.swift

---
+++ Step by Step/View/Story Home Views/StoryHomeNavigationButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  StoryHomeNavigationButtonView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/8/24.
+//
+
+import SwiftUI
+
+struct StoryHomeNavigationButtonView: View {
+    var buttonText: String
+    var buttonAction: (() -> Void)?
+    
+    var body: some View {
+        Button(action: {
+            buttonAction?()
+        }) {
+            Text(buttonText)
+                .font(.title3)
+                .bold()
+                .padding()
+                .frame(width: 250)
+                .background(Color.blue)
+                .foregroundStyle(.white)
+                .cornerRadius(20)
+        }
+    }
+}
+
+#Preview {
+    StoryHomeNavigationButtonView(
+        buttonText: "Return to Dashboard",
+        buttonAction: {}
+    )
+}
