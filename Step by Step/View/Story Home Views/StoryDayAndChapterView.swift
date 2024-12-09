9074d47e13582c949fbbb4f2af77a610d1bd118d
Gustavo Vazquez <great.visions@icloud.com>
2024-12-08 21:40:10 -0800

    Create StoryDayAndChapterView.swift

---
+++ Step by Step/View/Story Home Views/StoryDayAndChapterView.swift
@@ -0,0 +1,36 @@
+//
+//  StoryDayAndChapterView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/8/24.
+//
+
+import SwiftUI
+
+struct StoryDayAndChapterView: View {
+    var storyDay: Int
+    var storyTotalDays: Int
+    var storyChapter: String
+    
+    var body: some View {
+        // Day tracker
+        VStack(spacing: 10) {
+            Text("Day \(storyDay) out of \(storyTotalDays)")
+                .font(.title)
+                .fontWeight(.medium)
+            
+            // Current Chapter Title
+            Text("\"\(storyChapter)\"")
+                .font(.title2)
+                .fontWeight(.medium)
+        }
+    }
+}
+
+#Preview {
+    StoryDayAndChapterView(
+        storyDay: 3,
+        storyTotalDays: 10,
+        storyChapter: "Found Shelter"
+    )
+}
