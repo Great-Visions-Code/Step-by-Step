61ce80c883e3660b0639f5761a8d78be04e72818
Gustavo Vazquez <great.visions@icloud.com>
2024-12-08 21:39:58 -0800

    Create AttemptTrackerView.swift

---
+++ Step by Step/View/Story Home Views/AttemptTrackerView.swift
@@ -0,0 +1,23 @@
+//
+//  AttemptTrackerView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/8/24.
+//
+
+import SwiftUI
+
+struct AttemptTrackerView: View {
+    var newStoryAttempt: Int = 3
+    
+    var body: some View {
+        Text("Attempt #\(newStoryAttempt)")
+            .font(.title2)
+            .fontWeight(.bold)    }
+}
+
+#Preview {
+    AttemptTrackerView(
+        newStoryAttempt: 3
+    )
+}
