303338fce97063dae636b5e23f919a675c936d01
Gustavo Vazquez <great.visions@icloud.com>
2024-12-09 22:21:56 -0800

    Create StoryAchievementsView.swift

---
+++ Step by Step/View/Story Home Views/StoryAchievementsView.swift
@@ -0,0 +1,18 @@
+//
+//  StoryAchievementsView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/9/24.
+//
+
+import SwiftUI
+
+struct StoryAchievementsView: View {
+    var body: some View {
+        Text("Story Achievements View Placeholder")
+    }
+}
+
+#Preview {
+    StoryAchievementsView()
+}
