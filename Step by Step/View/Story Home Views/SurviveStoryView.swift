12eddc2c82cedbf6af1eaef2036d4c864ebe34cd
Gustavo Vazquez <great.visions@icloud.com>
2024-12-09 22:22:05 -0800

    Create SurviveStoryView.swift

---
+++ Step by Step/View/Story Home Views/SurviveStoryView.swift
@@ -0,0 +1,18 @@
+//
+//  SurviveStory.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/9/24.
+//
+
+import SwiftUI
+
+struct SurviveStoryView: View {
+    var body: some View {
+        Text("Survive Story View Placeholder")
+    }
+}
+
+#Preview {
+    SurviveStoryView()
+}
