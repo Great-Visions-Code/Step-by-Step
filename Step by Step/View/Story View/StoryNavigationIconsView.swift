29b8d2039d3d6ebc1f7fbb03527d257be9c7f006
Gustavo Vazquez <great.visions@icloud.com>
2024-12-15 20:45:39 -0800

    Create StoryNavigationIconsView.swift
    Refactor code

---
+++ Step by Step/View/Story View/StoryNavigationIconsView.swift
@@ -0,0 +1,42 @@
+//
+//  StoryNavigationIconsView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/15/24.
+//
+
+import SwiftUI
+
+struct StoryNavigationIconsView: View {
+    var body: some View {
+        HStack {
+            // Navigation icons
+            Button(action: {
+                // Navigate back to DashboardView()
+            }) {
+                Image(systemName: "house.fill")
+                    .font(.title2)
+                    .padding()
+            }
+            Spacer()
+            HStack(spacing: 20) {
+                Button(action: {
+                    // Navigate to StoryAchievementsView()
+                }) {
+                    Image(systemName: "star.fill")
+                        .font(.title2)
+                }
+                Button(action: {
+                    // Navigate to StoryMapView()
+                }) {
+                    Image(systemName: "map.fill")
+                        .font(.title2)
+                }
+            }
+            .padding()
+        }    }
+}
+
+#Preview {
+    StoryNavigationIconsView()
+}
