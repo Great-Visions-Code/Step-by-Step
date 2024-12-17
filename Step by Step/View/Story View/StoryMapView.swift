d9338f1f0076ba2a6f8039c83fdcb4d7a891b226
Gustavo Vazquez <great.visions@icloud.com>
2024-12-16 23:03:21 -0800

    Create StoryMapView.swift

---
+++ Step by Step/View/Story View/StoryMapView.swift
@@ -0,0 +1,18 @@
+//
+//  StoryMapView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/16/24.
+//
+
+import SwiftUI
+
+struct StoryMapView: View {
+    var body: some View {
+        Text("Story Map View Placeholder")
+    }
+}
+
+#Preview {
+    StoryMapView()
+}
