29ccddeb153e651651e4000147d4a38264285fc1
Gustavo Vazquez <great.visions@icloud.com>
2024-12-21 14:19:11 -0800

    Create StoryContent.swift
    Model

---
+++ Step by Step/Model/StoryContent.swift
@@ -0,0 +1,23 @@
+//
+//  StoryContent.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/21/24.
+//
+
+import Foundation
+import SwiftUI
+
+/// Model representing the content of a chapter within a story.
+struct StoryContent {
+    /// The in-game day associated with this chapter.
+    let storyDay: Int
+    /// The title of the chapter.
+    let chapterTitle: String
+    /// An array of image names or URL's for the chapter.
+    let chapterImages: [String]
+    /// The narrative text for the chapter.
+    let chapterText: String
+    /// The decisions available to the player, along with their associated next chapter IDs.
+    let chapterDecisions: [ChapterDecision]
+}
