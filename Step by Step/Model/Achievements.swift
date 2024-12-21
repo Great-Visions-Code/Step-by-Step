6470254561974d7291af55a57b651ce7013c7ead
Gustavo Vazquez <great.visions@icloud.com>
2024-12-20 23:02:53 -0800

    Create Achievements.swift
    Model

---
+++ Step by Step/Model/Achievements.swift
@@ -0,0 +1,15 @@
+//
+//  Achievements.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/20/24.
+//
+
+import Foundation
+import SwiftUI
+
+/// Model representing user achievements in the app.
+struct Achievements {
+    /// Tracks the number of attempts made by the user in the stories
+    var attempts: Int
+}
