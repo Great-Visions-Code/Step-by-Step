8891856f69ef438f1176b5c493031b2d0aa47474
Gustavo Vazquez <great.visions@icloud.com>
2024-12-21 14:18:21 -0800

    Create ChapterDecision.swift
    Model

---
+++ Step by Step/Model/ChapterDecision.swift
@@ -0,0 +1,16 @@
+//
+//  ChapterDecision.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 12/21/24.
+//
+
+import Foundation
+import SwiftUI
+
+/// Model representing a decision in a chapter, including tis description and the next chapter ID.
+struct ChapterDecision {
+    let decisionText: String
+    /// The ID of the next chapter this decision leads to.
+    let nextChapterID: Int
+}
