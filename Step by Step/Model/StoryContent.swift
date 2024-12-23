1200c559431fb4b31ad17216e3e0eec2d7a6c4e2
Gustavo Vazquez <great.visions@icloud.com>
2024-12-22 16:49:36 -0800

    Update StoryContent.swift
    Create chapterID for better navigation flow

--- Step by Step/Model/StoryContent.swift
+++ Step by Step/Model/StoryContent.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 /// Model representing the content of a chapter within a story.
 struct StoryContent {
+    /// A unique identifier for the chapter.
+    let chapterID: Int
     /// The in-game day associated with this chapter.
     let storyDay: Int
     /// The title of the chapter.
@@ -20,4 +22,6 @@ struct StoryContent {
     let chapterText: String
     /// The decisions available to the player, along with their associated next chapter IDs.
     let chapterDecisions: [ChapterDecision]
+    /// Indicates if this chapter is a terminal chapter (e.g., "Death" or "Survive").
+    let isFinalChapter: Bool
 }
