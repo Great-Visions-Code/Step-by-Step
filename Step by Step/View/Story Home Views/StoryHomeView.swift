2c19507194700e6b1daba415d4b317b552e68adc
Gustavo Vazquez <great.visions@icloud.com>
2024-12-28 22:35:19 -0800

    Update StoryHomeView.swift
    Call playerStatsViewModel.resetHealth() in Start New Story button selection

--- Step by Step/View/Story Home Views/StoryHomeView.swift
+++ Step by Step/View/Story Home Views/StoryHomeView.swift
@@ -87,7 +87,8 @@ struct StoryHomeView: View {
                                 storyContentViewModel.resumeStory()
                             } else if button.action == "StoryView" {
                                 // If the button is for starting a new story, reset the story to its initial state.
-                                storyContentViewModel.resetStory()
+                                storyContentViewModel.resetStory() // Start story from 1st chapter, day 1.
+                                playerStatsViewModel.resetHealth() // Reset health to full.
                             }
                             // Trigger navigation to the appropriate view using the button's action property.
                             onNavigateButton(button.action)
