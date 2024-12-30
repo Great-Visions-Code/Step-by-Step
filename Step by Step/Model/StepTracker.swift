f7b8d3de43ef0bcb2945712b679a8702b09b2d30
Gustavo Vazquez <great.visions@icloud.com>
2024-12-29 21:49:35 -0800

    Add healthKitImportedCurrentStepsTaken parameter

--- Step by Step/Model/StepTracker.swift
+++ Step by Step/Model/StepTracker.swift
@@ -9,19 +9,35 @@ import Foundation
 import SwiftUI
 
 /// Model representing the user's step tracking progress and goals.
+/// This struct handles the user's daily step goals and tracks steps taken,
+/// as well as providing utilities to calculate progress and reset steps.
 struct StepTracker {
-    var currentStepsTaken: Int
+    /// The number of steps available for conversion into energy points.
+    var stepsToConvert: Int
+    
+    /// The total number of steps required to meet the user's daily goal.
     var totalStepsGoals: Int
     
+    /// The current number of steps taken as reported by HealthKit or a similar step-tracking service.
+    var healthKitImportedCurrentStepsTaken: Int
+    
+    // TODO: To be used in CurrentStepsTakenView.swift
     /// Calculate the user's progress as a percentage.
-    /// - Returns: A value between 0 and 100  representing the progress percentage.
+    ///
+    /// This function calculates the progress towards the daily step goal.
+    /// If the `totalStepsGoals` is 0, it safely returns 0 to avoid division by zero errors.
+    ///
+    /// - Returns: A value between 0 and 100 representing the progress percentage.
     func progressPercentage() -> Double {
-        guard totalStepsGoals > 0 else { return 0 }
-        return (Double(currentStepsTaken) / Double(totalStepsGoals)) * 100
+        guard totalStepsGoals > 0 else { return 0 } // Ensure a valid goal is set.
+        return (Double(stepsToConvert) / Double(totalStepsGoals)) * 100
     }
     
-    /// Resets the current steps to zero.
+    /// Resets the current steps available for conversion to zero.
+    ///
+    /// This method is typically used when the steps have been converted into energy
+    /// or at the start of a new tracking period.
     mutating func resetSteps() {
-        currentStepsTaken = 0
+        stepsToConvert = 0
     }
 }
