16f92ca4fd8c3c6498a168ce93f2ccf5e558ef0c
Gustavo Vazquez <great.visions@icloud.com>
2025-01-05 17:11:00 -0800

    Update StoryChapterContentView.swift
    Update ScrollView to snap to top on every chapter.

--- Step by Step/View/Story Views/StoryView SubViews/StoryChapterContentView.swift
+++ Step by Step/View/Story Views/StoryView SubViews/StoryChapterContentView.swift
@@ -14,62 +14,77 @@ struct StoryChapterContentView: View {
     @ObservedObject var storyContentViewModel: StoryContentViewModel
     /// ViewModel to manage the player's stats, such as health and energy.
     @ObservedObject var playerStatsViewModel: PlayerStatsViewModel
+    
+    /// Unique ID for resetting the scroll position.
+    @State private var scrollAnchorID = "ScrollToTop"
 
     var body: some View {
-        ScrollView {
-            if let currentChapter = storyContentViewModel.currentChapter {
+        ScrollViewReader { proxy in
+            ScrollView {
                 VStack(spacing: 20) {
-                    // Displays the chapter title for the current story section.
-                    Text(currentChapter.chapterTitle)
-                        .font(.title)
-                        .bold()
-                    
-                    // Display the first image in the chapter, if available.
-                    if let imageName = currentChapter.chapterImages.first {
-                        Image(imageName)
-                            .resizable()
-                            .scaledToFit()
+                    if let currentChapter = storyContentViewModel.currentChapter {
+                        // Displays the chapter title for the current story section.
+                        Text(currentChapter.chapterTitle)
+                            .font(.title)
+                            .bold()
+                            .id(scrollAnchorID) // Anchor to scroll to the top.
+                        
+                        // Display the first image in the chapter, if available.
+                        if let imageName = currentChapter.chapterImages.first {
+                            Image(imageName)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
+                        // Displays the narrative content for the chapter.
+                        Text(currentChapter.chapterText)
+                            .font(.body)
+                            .padding()
+                        
+                        Spacer()
+                        
+                        // Buttons for user decisions related to the current chapter.
+                        StoryDecisionButton(
+                            options: currentChapter.chapterDecisions.compactMap { decision in
+                                if let nextChapterID = decision.nextChapterID {
+                                    return (
+                                        title: decision.decisionText,
+                                        HPChange: decision.HPChange,
+                                        EPChange: decision.EPChange,
+                                        action: {
+                                            // Update the story chapter when the button is pressed.
+                                            storyContentViewModel.updateCurrentChapter(
+                                                to: nextChapterID,
+                                                HPChange: decision.HPChange,
+                                                EPChange: decision.EPChange
+                                            )
+                                        }
+                                    )
+                                } else {
+                                    return nil
+                                }
+                            },
+                            currentEnergy: playerStatsViewModel.playerStats.energy
+                        )
+                    } else {
+                        // Fallback UI if no chapter is loaded.
+                        Text("No Chapter Available")
+                            .font(.title)
+                            .padding()
                     }
-                    
-                    // Displays the narrative content for the chapter.
-                    Text(currentChapter.chapterText)
-                        .font(.body)
-                        .padding()
-                    
-                    Spacer()
-                    
-                    // Buttons for user decisions related to the current chapter.
-                    StoryDecisionButton(
-                        options: currentChapter.chapterDecisions.compactMap { decision in
-                            // Check if the decision has a valid nextChapterID.
-                            if let nextChapterID = decision.nextChapterID {
-                                // Return a tuple containing the decision details to create a button.
-                                return (
-                                    title: decision.decisionText, // The text describing the decision.
-                                    HPChange: decision.HPChange, // The health change associated with the decision.
-                                    EPChange: decision.EPChange, // The energy change associated with the decision.
-                                    action: {
-                                        // Update the story chapter when the button is pressed.
-                                        storyContentViewModel.updateCurrentChapter(
-                                            to: nextChapterID, // The chapter to navigate to after making the decision.
-                                            HPChange: decision.HPChange, // Apply health change based on the decision.
-                                            EPChange: decision.EPChange // Apply energy change based on the decision.
-                                        )
-                                    }
-                                )
-                            } else {
-                                // If the decision doesn't have a valid nextChapterID, exclude it.
-                                return nil
-                            }
-                        },
-                        currentEnergy: playerStatsViewModel.playerStats.energy // Pass current energy directly
-                    )
                 }
-            } else {
-                // Fallback UI if no chapter is loaded.
-                Text("No Chapter Available")
-                    .font(.title)
-                    .padding()
+            }
+            // Observes changes to the `currentChapter` property in the `storyContentViewModel`.
+            // This modifier triggers whenever the current chapter is updated, allowing
+            // the UI to respond dynamically to changes in the story.
+            .onChange(of: storyContentViewModel.currentChapter, initial: false) { _, _ in
+                // Animates the scrolling action to the top of the scroll view.
+                // The `proxy.scrollTo` function uses `scrollAnchorID` to identify the top
+                // of the scrollable content, ensuring the user starts reading the new chapter
+                // from the beginning.
+                withAnimation {
+                    proxy.scrollTo(scrollAnchorID, anchor: .top)
+                }
             }
         }
     }
@@ -81,6 +96,6 @@ struct StoryChapterContentView: View {
             achievementsViewModel: AchievementsViewModel(),
             playerStatsViewModel: PlayerStatsViewModel()
         ),
-        playerStatsViewModel: PlayerStatsViewModel() // Pass PlayerStatsViewModel to track active/disable decision buttons
+        playerStatsViewModel: PlayerStatsViewModel()
     )
 }
