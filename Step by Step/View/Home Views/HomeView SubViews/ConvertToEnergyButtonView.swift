f684b2c6bed412b137e4e6c331f7ad0df00bc96a
Gustavo Vazquez <great.visions@icloud.com>
2025-02-07 22:49:56 -0800

    Update ConvertToEnergyButtonView.swift
    Update design

--- Step by Step/View/Home Views/HomeView SubViews/ConvertToEnergyButtonView.swift
+++ Step by Step/View/Home Views/HomeView SubViews/ConvertToEnergyButtonView.swift
@@ -19,34 +19,36 @@ struct ConvertToEnergyButtonView: View {
     @ObservedObject var stepTrackerViewModel: StepTrackerViewModel
     
     var body: some View {
-        // A button that performs the steps-to-energy conversion and resets steps.
-        Button(action: {
-            // Calculate energy points based on the user's progress toward their step goal in ViewModel.
-            // Add calculatedEnergyPoints to current energy.
-            let newEnergy = stepTrackerViewModel.calculateEnergyPoints() + playerStatsViewModel.playerStats.energy
-            
-            // Update the energy points in the player stats ViewModel.
-            playerStatsViewModel.updateEnergy(to: newEnergy)
-            
-            // Reset the current steps in ViewModel.
-            stepTrackerViewModel.resetStepsToConvert()
-        }) {
-            VStack(spacing: 15) {
-                Text("Convert")
-                    .font(.title2)
-                HStack(spacing: 10) {
-                    Text("\(stepTrackerViewModel.stepTracker.stepsToConvert) steps")
-                        .foregroundStyle(.yellow)
-                    Image(systemName: "arrow.right.circle.fill")
-                    Text("To Energy")
+        VStack {
+            // A button that performs the steps-to-energy conversion and resets steps.
+            Button(action: {
+                // Calculate energy points based on the user's progress toward their step goal in ViewModel.
+                // Add calculatedEnergyPoints to current energy.
+                let newEnergy = stepTrackerViewModel.calculateEnergyPoints() + playerStatsViewModel.playerStats.energy
+                
+                // Update the energy points in the player stats ViewModel.
+                playerStatsViewModel.updateEnergy(to: newEnergy)
+                
+                // Reset the current steps in ViewModel.
+                stepTrackerViewModel.commitStepsToTotal()
+            }) {
+                VStack {
+                    HStack {
+                        Text("Convert")
+                            .font(.title2)
+                        Text(" \(stepTrackerViewModel.stepTracker.stepsToConvert) steps")
+                            .font(.title3)
+                    }
+  
                 }
+                .bold()
+                .padding()
+                .background(Color.blue)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
             }
-            .font(.title3)
-            .bold()
-            .padding()
-            .background(Color.blue)
-            .foregroundStyle(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
+            Text("Tap to convert steps into energy")
+                .font(.footnote)
         }
     }
 }
