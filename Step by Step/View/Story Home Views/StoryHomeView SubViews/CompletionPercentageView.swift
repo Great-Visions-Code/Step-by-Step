59f8f51b30fd9c865380a9013a5dbd5c13afbafa
Gustavo Vazquez <great.visions@icloud.com>
2025-02-20 22:34:30 -0800

    Create CompletionPercentageView.swift

---
+++ Step by Step/View/Story Home Views/StoryHomeView SubViews/CompletionPercentageView.swift
@@ -0,0 +1,38 @@
+//
+//  CompletionPercentageView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 2/19/25.
+//
+
+import SwiftUI
+
+struct CompletionPercentageView: View {
+    // ViewModel to manage the story content.
+    @ObservedObject var storyContentViewModel: StoryContentViewModel
+    
+    var body: some View {
+        VStack {
+            Text("Completed: \(storyContentViewModel.completionPercentage)%")
+                .font(.title3)
+                .fontWeight(.semibold)
+            
+            // Modern thin progress bar
+            ProgressView(value: Double(storyContentViewModel.completionPercentage), total: 100)
+                .progressViewStyle(LinearProgressViewStyle(tint: Color.blue))
+                .frame(height: 6) // Thin sleek progress bar
+                .background(Color.gray.opacity(0.3)) // Subtle background for contrast
+                .cornerRadius(3) // Rounded edges for a modern look
+                .padding(.horizontal, 80) // Centering the bar
+        }
+        .padding(.bottom, 20)
+    }
+}
+
+#Preview {
+    CompletionPercentageView(storyContentViewModel: StoryContentViewModel(
+        achievementsViewModel: AchievementsViewModel(),
+        playerStatsViewModel: PlayerStatsViewModel()
+        )
+    )
+}
