af50bd1c1b872b75db4aba9af3d0f29b2a18880d
Gustavo Vazquez <great.visions@icloud.com>
2025-03-14 22:21:21 -0700

    Create StepActivityView.swift

---
+++ Step by Step/View/Achievements Views/Step Activity Views/StepActivityView.swift
@@ -0,0 +1,22 @@
+//
+//  StepActivityView.swift
+//  Step by Step
+//
+//  Created by Gustavo Vazquez on 3/14/25.
+//
+
+import SwiftUI
+
+struct StepActivityView: View {
+    var body: some View {
+        VStack {
+            Text("Step Activity View Placeholder")
+                .foregroundColor(.gray)
+                .padding(.top, 50)
+        }
+    }
+}
+
+#Preview {
+    StepActivityView()
+}
