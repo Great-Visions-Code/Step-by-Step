75aa047532ce09883836e6eab73596d9b39a0990
Gustavo Vazquez <great.visions@icloud.com>
2025-03-16 14:29:33 -0700

    Update DashboardView.swift
    Rename Achievements vs to StatsView

--- Step by Step/View/DashboardView.swift
+++ Step by Step/View/DashboardView.swift
@@ -38,7 +38,7 @@ struct DashboardView: View {
     var body: some View {
         TabView(selection: $selectedTab) {
             // Achievements tab for showcasing user accomplishments.
-            AchievementsView()
+            StatsView()
             .tabItem {
                 Image(systemName: "figure.walk")
                 Text("Stats")
