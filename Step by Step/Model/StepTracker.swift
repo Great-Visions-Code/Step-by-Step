423fdcb2cb5b7ea805bd8aa6ecb8476f4fa3204b
Gustavo Vazquez <great.visions@icloud.com>
2025-03-17 22:02:00 -0700

    Update StepTracker.swift

--- Step by Step/Model/StepTracker.swift
+++ Step by Step/Model/StepTracker.swift
@@ -14,30 +14,24 @@ import Foundation
 struct StepTracker {
     /// The current step count.
     var currentStepCount: Int
-    
     /// The total number of steps required to meet the user's daily goal.
     var totalStepsGoal: Int
-    
     /// The current distance traveled data. 
     var currentDistance: Double
-    
     /// The total number of steps taken. 
     var totalStepsTaken: Int {
         didSet {
             StepTrackerViewModel.saveTotalStepsTaken(totalStepsTaken) // Persist when updated
         }
     }
-    
     /// The number of steps available for conversion into energy points.
     ///
     /// This value is calculated as `currentStepCount - totalStepsTaken`.
     var stepsToConvert: Int {
         max(currentStepCount - totalStepsTaken, 0)
     }
-    
     /// The average step count for the past 7-days.
     var sevenDayStepAverage: Double = 0.0
-    
     /// The daily step history for the past 7-days.
     var stepHistory: [(date: String, steps: Int)] = []
 }
