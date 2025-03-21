56457c957c2f0321e7273aed7ffeb6fbc4172e8b
Gustavo Vazquez <great.visions@icloud.com>
2025-03-20 20:10:32 -0700

    Fix layout

--- Step by Step/View/Stats Views/Step Activity SubViews/StepActivityView.swift
+++ Step by Step/View/Stats Views/Step Activity SubViews/StepActivityView.swift
@@ -11,17 +11,14 @@ struct StepActivityView: View {
 
     var body: some View {
         VStack(spacing: 40) {
-            Spacer()
-            
             // MARK: - Steps Taken Stats View
             StepsTakenStatsView(
                 stepTrackerViewModel: StepTrackerViewModel()
             )
-                        
             // MARK: - Step Stats Graph View
             StepStatsGraphView(stepTrackerViewModel: StepTrackerViewModel())
         }
-        .padding(.bottom, 100)
+        .padding(.bottom, 75)
     }
 }
 
