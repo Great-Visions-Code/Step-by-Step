df75b934f4c5a597e20cbfad0cc12f8638da9e82
Gustavo Vazquez <great.visions@icloud.com>
2025-06-03 17:05:18 -0700

    Update StoryHomeView.swift
    Add background image

--- Step by Step/View/Story Home Views/StoryHomeView.swift
+++ Step by Step/View/Story Home Views/StoryHomeView.swift
@@ -32,78 +32,81 @@ struct StoryHomeView: View {
     ]
     
     var body: some View {
-        VStack {
-            Spacer() // Spacer to push content towards the center of the screen.
+        ZStack {
+            Image("survive-day1-homeImage")
+                .resizable()
+                .ignoresSafeArea()
             
-            // Display the title of the current story.
-            StoryTitleView(
-                title: story.storyTitle
-            )
-            .padding()
-
-            // Show the player's current health and energy levels.
-            VStack(spacing: 20) {
-                StoryHPAndEPBarView(
-                    currentPoints: playerStatsViewModel.playerStats.health,
-                    maxPoints: 10,
-                    type: .health,
-                    iconSpacing: 8
-                )
-                StoryHPAndEPBarView(
-                    currentPoints: playerStatsViewModel.playerStats.energy,
-                    maxPoints: 10,
-                    type: .energy,
-                    iconSpacing: 14
-                )
-            }
-            .padding()
-            
-            // Display the current day and chapter of the story, along with the number of attempts.
             VStack {
-                StoryDayAndChapterView(
-                    storyContentViewModel: storyContentViewModel
-                )
-                .padding(.bottom, 20)
+                Spacer() // Spacer to push content towards the center of the screen.
                 
-                // Display Survive completion percentage dynamically.
-                CompletionPercentageView(storyContentViewModel: StoryContentViewModel(
-                    achievementsViewModel: AchievementsViewModel(),
-                    playerStatsViewModel: PlayerStatsViewModel()
+                // MARK: - Display the title of the current story.
+                StoryTitleView(title: story.storyTitle)
+                    .padding()
+                    
+                
+                // MARK: - Show the player's current health and energy levels.
+                VStack(spacing: 20) {
+                    StoryHPAndEPBarView(
+                        currentPoints: playerStatsViewModel.playerStats.health,
+                        maxPoints: 10,
+                        type: .health,
+                        iconSpacing: 8
                     )
-                )
-                
-                // Display Attempts taken.
-                AttemptTrackerView(
-                    achievementsViewModel: achievementsViewModel
-                )
-               // .padding()
-            }
-            .padding()
-
-            // Provide navigation buttons to resume, start a new story, or view achievements.
-            VStack(spacing: 20) {
-                // Loop through each button configuration in `buttonActions`.
-                ForEach(buttonActions, id: \.title) { button in
-                    // Create a navigation button for each action.
-                    StoryHomeNavigationButtonView(
-                        buttonText: button.title, // The text displayed on the button.
-                        buttonAction: {
-                            // Determine the action to take based on the button's `action` property.
-                            if button.action == "ResumeStoryView" {
-                                // If the button is for resuming the story, call the resumeStory method.
-                                storyContentViewModel.resumeStory()
-                            } else if button.action == "StoryView" {
-                                // If the button is for starting a new story, reset the story to its initial state.
-                                storyContentViewModel.resetStory() // Start story from beginning.
-                                playerStatsViewModel.resetHealth() // Reset health to full.
-                            }
-                            // Trigger navigation to the appropriate view using the button's action property.
-                            onNavigateButton(button.action)
-                        }
+                    StoryHPAndEPBarView(
+                        currentPoints: playerStatsViewModel.playerStats.energy,
+                        maxPoints: 10,
+                        type: .energy,
+                        iconSpacing: 14
                     )
                 }
+                .padding()
+                
+                // MARK: - Display the current day and chapter of the story, along with the number of attempts.
+                VStack {
+                    StoryDayAndChapterView(storyContentViewModel: storyContentViewModel)
+                    .padding(.bottom, 20)
+                    
+                    // MARK: - Display Survive completion percentage dynamically.
+                    CompletionPercentageView(storyContentViewModel: StoryContentViewModel(
+                        achievementsViewModel: AchievementsViewModel(),
+                        playerStatsViewModel: PlayerStatsViewModel()
+                        )
+                    )
+                    
+                    // MARK: - Display Attempts taken.
+                    AttemptTrackerView(
+                        achievementsViewModel: achievementsViewModel
+                    )
+                    .padding()
+                }
+                .padding()
+                
+                // MARK: - Provide navigation buttons to resume, start a new story, or view achievements.
+                VStack(spacing: 20) {
+                    // Loop through each button configuration in `buttonActions`.
+                    ForEach(buttonActions, id: \.title) { button in
+                        // Create a navigation button for each action.
+                        StoryHomeNavigationButtonView(
+                            buttonText: button.title, // The text displayed on the button.
+                            buttonAction: {
+                                // Determine the action to take based on the button's `action` property.
+                                if button.action == "ResumeStoryView" {
+                                    // If the button is for resuming the story, call the resumeStory method.
+                                    storyContentViewModel.resumeStory()
+                                } else if button.action == "StoryView" {
+                                    // If the button is for starting a new story, reset the story to its initial state.
+                                    storyContentViewModel.resetStory() // Start story from beginning.
+                                    playerStatsViewModel.resetHealth() // Reset health to full.
+                                }
+                                // Trigger navigation to the appropriate view using the button's action property.
+                                onNavigateButton(button.action)
+                            }
+                        )
+                    }
+                }
+                Spacer() // Spacer to balance layout and push buttons toward the bottom.
             }
-            Spacer() // Spacer to balance layout and push buttons toward the bottom.
         }
         // Hide the default navigation back button since this is a top-level view.
         .navigationBarBackButtonHidden(true)
